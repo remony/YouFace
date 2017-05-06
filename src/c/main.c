56a5339a0c0f3b23861cfa27bd43ad6e90998d2b
remony <stuartjdouglas@icloud.com>
2017-05-06 18:39:22 +0100

    added background image and timelayer

--- src/c/main.c
+++ src/c/main.c
@@ -1,23 +1,71 @@
 #include <pebble.h>
 static Window *s_main_window;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? 
+           "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+  
+
+static void timeLayerHandler() {
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+}
 
 static void main_window_load(Window *window) {
+  // Window information  
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
   
+  // create our base bitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_YOU_BASE);
+  s_background_layer = bitmap_layer_create(bounds);
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+  
+  // Creation of time layer
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(48, 8), bounds.size.w, 50));
+  timeLayerHandler();
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-  
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void init() {
   s_main_window = window_create();
+  
+  // register services
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
   
+  window_set_background_color(s_main_window, GColorWhite);
   window_stack_push(s_main_window, true);
+  update_time();
 }
 
 static void deinit() {
